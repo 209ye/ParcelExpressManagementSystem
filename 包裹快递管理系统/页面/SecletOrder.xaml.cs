f62cb4c4eb0f69fca7c9c6de7d34d14b873d89fc
209ye <209ye@users.noreply.github.com>
2020-05-27 11:14:01 +0800

    2020.05.27 11:13

--- 包裹快递管理系统/页面/SecletOrder.xaml.cs
+++ 包裹快递管理系统/页面/SecletOrder.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
 
 namespace 包裹快递管理系统
 {
@@ -21,12 +22,62 @@ namespace 包裹快递管理系统
     /// </summary>
     public partial class SecletOrder : UserControl
     {
+        MySQLConnect connect = new MySQLConnect();
+
         public SecletOrder()
         {
             List<SelOrder> selOrder = new List<SelOrder>();
-            selOrder.Add(new SelOrder("1", "1", "1", "1", "1", "1", "1", "1", "1", "1"));
+            //DataTable dt = new DataTable();
+
+            try
+            {
+                //DataTable的实现方式，会把顶部的表头覆盖掉
+                //MySqlDataAdapter da = new MySqlDataAdapter(str_sql, connect.MySqlConnection);
+
+                //da.Fill(dt);
+
+                connect.MySqlConnection.Open();
+                connect.ConnectIndex = true;
+                //查询语句
+                /*                String str_sql = "select mail_id , courier_company , org_name , org_address , " +
+                                                 "send_name ,  send_address , send_contact , stats , remarks" +
+                                                 "from fact_mail_status";*/
+                String str_sql = "select order_id, order_list.mail_id ,courier_company , org_name , org_address, send_name ,  send_address , send_contact,stats ,remarks from fact_mail_status inner join order_list on fact_mail_status.mail_id = order_list.mail_id";
+                MySqlCommand mysql_cmd = new MySqlCommand(str_sql, connect.MySqlConnection);
+                MySqlDataReader reader = mysql_cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    String order_id = reader["order_id"].ToString();
+                    String mail_id = reader["mail_id"].ToString();
+                    String courier_company = reader["courier_company"].ToString();
+                    String org_name = reader["org_name"].ToString();
+                    String org_address = reader["org_address"].ToString();
+                    String send_name = reader["send_name"].ToString();
+                    String send_address = reader["send_address"].ToString();
+                    String send_contact = reader["send_contact"].ToString();
+                    String stats = reader["stats"].ToString();
+                    String remarks = reader["remarks"].ToString();
+                    /*  selOrder.Add(new SelOrder("1", mail_id, courier_company, org_name, org_address,
+                          send_name, send_address, send_contact, stats, remarks));*/
+                    selOrder.Add(new SelOrder(order_id, mail_id, courier_company, org_name, org_address,
+                        send_name, send_address, send_contact, stats, remarks));
+                }
+                reader.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //关闭游标
+                connect.MySqlConnection.Close();
+            }
             InitializeComponent();
-            selectOrder.ItemsSource= selOrder;
+            //selectOrder.ItemsSource = dt.DefaultView;
+            selectOrder.ItemsSource = selOrder;
         }
     }
 }
+
